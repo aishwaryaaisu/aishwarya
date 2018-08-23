cf1475dd4f39c93fcdd5c473f1f185f4e39a5f00
aishwaryaaisu <aishwaryaaisu0131@gmail.com>
2018-08-23 22:40:09 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+from bottle import route, run, jinja2_view,request
+
+@route('/')
+@jinja2_view('index.html')
+def index():
+	return {'name':'aish'}
+
+@route('/',method="post")
+@jinja2_view('index.html')
+def form_handler():
+	para= request.forms.get('para')
+	old= request.forms.get('old')
+	new= request.forms.get('new')
+	new_para=para.replace(old,new)
+	return {"para":new_para}
+
+run(host='0.0.0.0',port=int(os.environ.get("PORT",5000)))
